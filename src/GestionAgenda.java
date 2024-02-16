fb1bec18ab6bc3b183c8c5a8ccd1128e352a2dc0
14Math <tmatrix770@gmail.com>
2024-02-16 09:16:19 +0100

    (feat)estructura creada en el interface de la clase Gestionagenda

--- src/GestionAgenda.java
+++ src/GestionAgenda.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public interface GestionAgenda {
+
+    ArrayList<Contacto> buscarTodos();
+    boolean altacontacto(Contacto contacto );
+    boolean eliminarcontacto(Contacto contacto);
+    Contacto buscaruno(String nombre);
+    Contacto buscartelefono(String telefono);
+    Contacto buscaremail(String email);
+    ArrayList<Contacto> buscarContactosPorTresPrimeros(String nombre);
+    boolean cambiardatos(Contacto contacto);
+    ArrayList<Contacto> buscarContactosPorEmpresa(String empresa);
+    void exportarcontactos(String nombreFichero);
+    void importarcontactos(String nombreFichero);
 }
